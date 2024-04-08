daca4f12bd49f54b85e174cb68be2c05a646c268
Rajankumar Mandanka <119439834+rajanmandanka07@users.noreply.github.com>
2024-04-08 12:00:59 +0530

    Update AuthComponent.js

--- c/src/AuthComponent.js
+++ c/src/AuthComponent.js
@@ -7,7 +7,7 @@ const AuthComponent = () => {
 
     const handleRegister = async () => {
         try {
-            const response = await fetch('http://localhost:4000/register', {
+            const response = await fetch(`${process.env.REACT_APP_API_URL}/register`, {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
@@ -24,7 +24,7 @@ const AuthComponent = () => {
 
     const handleLogin = async () => {
         try {
-            const response = await fetch('http://localhost:4000/login', {
+            const response = await fetch(`${process.env.REACT_APP_API_URL}/login`, {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
